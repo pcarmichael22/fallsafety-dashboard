9530ccbc7d47154f3f2cc0e44d37306aed7fda85
Peter Carmichael <peter.carmichael22@gmail.com>
2019-07-09 11:49:41 -0700

    updated html file

---
+++ codefellows/201/lab-02/js/app.js
@@ -0,0 +1,7 @@
+var ultimateQuestion = prompt('What is the meaning of life? Be sure to spell it out!');
+
+if(ultimateQuestion.toLowerCase() === 'forty two'){
+    alert('You\'ve figured it out!');
+} else {
+    alert('please try again');
+}
